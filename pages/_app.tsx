18d398eb0e978886a3b1887b45ecfd34268667d3
Snoopsie1 <oliver.rasoli@gmail.com>
2022-05-05 13:13:52 +0200

    RealmList

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import '../styles/globals.css'
 import '../styles/HomePage.css'
 import type { AppProps } from 'next/app'
+import RealmListProvider from "../store/RealmContext/RealmList";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+      <RealmListProvider>
+        <Component {...pageProps} />
+      </RealmListProvider>
+      )
 }
 
 export default MyApp
