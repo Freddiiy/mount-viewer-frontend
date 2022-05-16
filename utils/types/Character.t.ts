4194ba8beec766d25afd4c92764214fdc8d97d29
Freddiiy <frederikgaller@live.dk>
2022-05-16 11:00:31 +0200

    Added fetch for mounts but need to make it grid

--- utils/types/Character.t.ts
+++ utils/types/Character.t.ts
@@ -1,14 +1,26 @@
 export interface ICharacter {
+	assets: Asset[];
 	id: number;
 	name: string;
 	level: number;
-	gender: Faction;
+	gender: Gender;
 	faction: Faction;
 	race: Race;
 	realm: Realm;
 }
 
+export interface Asset {
+	key: string;
+	value: string;
+	file_data_id: number;
+}
+
 export interface Faction {
+	type: string;
+	name: string;
+}
+
+export interface Gender {
 	type: string;
 	name: string;
 }
