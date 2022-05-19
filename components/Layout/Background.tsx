fe64ffb17269e32a84353187ec09f38d73176272
Freddiiy <frederikgaller@live.dk>
2022-05-19 15:30:59 +0200

    DONE AND NEW SPINNER and fixed some scrolling and background transparency issue

--- components/Layout/Background.tsx
+++ components/Layout/Background.tsx
@@ -2,18 +2,22 @@ import React, {ReactNode} from "react";
 import {Box} from "@chakra-ui/react";
 import {useAppSelector} from "../../store/hooks";
 
-export default function Background({children}: { children: ReactNode }) {
+export default function Background({children}: { children?: ReactNode }) {
 	const character = useAppSelector(state => state.character);
 	return (
 		<Box
+			bgColor={"transparent"}
 			px={8}
 			py={24}
 			mx={"auto"}
-			backgroundImage={character.value?.faction.type == "HORDE" ? "url(/horde-bg.png)" : "url(/allience-bg.png)"}
+			minHeight={"100vh"}
+			bgImage={character.value?.faction.type == "HORDE" ? "url(/horde-bg.png)" : "url(/allience-bg.png)"}
 			backgroundAttachment={"fixed"}
+			position={"relative"}
 			bgPos={"center"}
 			bgSize={"cover"}
-			overflow={"hidden"}>
+			overflow={"hidden"}
+		>
 			{children}
 		</Box>
 	)
